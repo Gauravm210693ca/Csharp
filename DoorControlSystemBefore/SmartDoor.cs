8ed55c3043add324f58ae82c860a414684914704
Gauravm210693ca <124175535+Gauravm210693ca@users.noreply.github.com>
2024-01-24 23:25:44 +0530

    Add files via upload

---
+++ DoorControlSystemBefore/SmartDoor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace DoorsControlSystem
+{
+    public class SmartDoor:SimpleDoor
+    {
+        
+        public event Action<DoorState> StateChanged;
+        public DateTime openedTime;
+        public DoorState currentState;
+        TimerManager timerManagerObj;
+        public SmartDoor(TimerManager timerManager)
+        {
+            openedTime = DateTime.Now;
+            timerManagerObj = timerManager; 
+        }
+        public override void Open()
+        {
+            
+            if (this.currentState == DoorState.CLOSED)
+            {
+                this.currentState = DoorState.OPENED;
+                openedTime = DateTime.Now;
+               
+            }
+            
+        }
+
+        public override void Close()
+        {
+            if (this.currentState == DoorState.OPENED)
+            {
+                this.currentState = DoorState.CLOSED;
+            }
+            
+        }
+       
+        public void Notify(bool value)
+        {
+            if (value == true)
+            {
+                StateChanged.Invoke(this.currentState);
+            }
+        }
+
+        
+    }
+}
