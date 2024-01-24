db83117207fa587c5701746bdcac8209290b9a9d
Gauravm210693ca <124175535+Gauravm210693ca@users.noreply.github.com>
2024-01-24 23:23:30 +0530

    Add files via upload

---
+++ DoorControlSystemBefore/BuzzerManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorsControlSystem
+{
+    public class BuzzerManager:INotifier
+    {
+        
+        private DoorState currentState;
+        public void Update(DoorState state)
+        {
+            this.currentState = state;
+            Console.WriteLine("Alert!!!");
+        }
+        
+    }
+}
