aba0dd7ee33016d09f22fd9728106fc040aafce2
Gauravm210693ca <124175535+Gauravm210693ca@users.noreply.github.com>
2024-01-24 23:24:02 +0530

    Add files via upload

---
+++ DoorControlSystemBefore/PagerManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorsControlSystem
+{
+    public class PagerManager:INotifier
+    {
+        private DoorState currentState;
+        public void Update(DoorState state)
+        {
+            this.currentState = state;
+            Console.WriteLine("Pager");
+        }
+    }
+}
