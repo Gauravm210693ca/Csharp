9c67cd204ff41c777ca6322ac7d6fa76e86f9493
Gauravm210693ca <124175535+Gauravm210693ca@users.noreply.github.com>
2024-01-24 23:22:52 +0530

    Add files via upload

---
+++ DoorControlSystemBefore/AutoCloseManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorsControlSystem
+{
+    public class AutoCloseManager: INotifier
+    {
+        
+        private DoorState currentState;
+        
+        public void Update(DoorState state)
+        {
+            this.currentState = state;
+            Console.WriteLine("AutoClose");
+
+        }
+        public void AutoClose()
+        {
+            if (this.currentState == DoorState.OPENED)
+            {
+                this.currentState = DoorState.CLOSED;
+            }
+
+        }        
+       
+    }
+}
