0655e9db732b61e69568c3532bb86ddbd134b965
Gauravm210693ca <124175535+Gauravm210693ca@users.noreply.github.com>
2024-01-28 21:10:18 +0530

    Add files via upload

---
+++ HTMLConvertor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordDocumentProgram
+{
+    public class HTMLConvertor:IConvertor
+    {
+        public void Convert(Paragraph paragraph)
+        {
+
+        }
+        public void Convert(Header header)
+        {
+
+        }
+        public void Convert(HyperLink hyperLink) { }
+        public void Convert(Footer footer) { }
+    }
+}
