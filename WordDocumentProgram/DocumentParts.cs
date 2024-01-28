eb9fe0dfbb3c008440ba539192968c1bc89db8e9
Gauravm210693ca <124175535+Gauravm210693ca@users.noreply.github.com>
2024-01-28 21:09:04 +0530

    Add files via upload

---
+++ WordDocumentProgram/DocumentParts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordDocumentProgram
+{
+    public abstract class DocumentParts
+    {
+        public string DocumentPartName;
+        
+        public abstract void save();
+        public abstract void paint();
+        public abstract void convert(IConvertor iConvertor);
+
+    }
+}
