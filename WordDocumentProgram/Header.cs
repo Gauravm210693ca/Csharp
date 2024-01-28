6cb0f6f37d7e33b39764da63767d36fb21c07684
Gauravm210693ca <124175535+Gauravm210693ca@users.noreply.github.com>
2024-01-28 21:10:00 +0530

    Add files via upload

---
+++ WordDocumentProgram/Header.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordDocumentProgram
+{
+    public class Header:DocumentParts
+    {
+        string title;
+        public override void save()
+        {
+            Console.WriteLine("Header saved");
+        }
+        public override void paint()
+        {
+            Console.WriteLine("Header painted");
+        }
+        public override void convert(IConvertor iconverot)
+        {
+            iconverot.Convert(this);
+        }
+    }
+}
