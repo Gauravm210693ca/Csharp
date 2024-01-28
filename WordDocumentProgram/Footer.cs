5363a8132fd309630543cad2869fd714eb81552c
Gauravm210693ca <124175535+Gauravm210693ca@users.noreply.github.com>
2024-01-28 21:09:38 +0530

    Add files via upload

---
+++ WordDocumentProgram/Footer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordDocumentProgram
+{
+    public class Footer:DocumentParts
+    {
+        string text;
+        public override void save()
+        {
+            Console.WriteLine("Footer saved");
+        }
+        public override void paint()
+        {
+            Console.WriteLine("Footer painted");
+        }
+        public override void convert(IConvertor iconvertor)
+        {
+            iconvertor.Convert(this);
+        }
+    }
+}
